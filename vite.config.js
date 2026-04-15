4c07b23679a01512b182eaba122b3b5f3f3b535e
duiliuliu <pengrui55555@163.com>
2026-04-15 01:03:04 +0000

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import logseqDevPlugin from 'vite-plugin-logseq'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
     root: 'src',
     plugins: [react(), logseqDevPlugin()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     build: {
       outDir: '../dist',
       emptyOutDir: true,
