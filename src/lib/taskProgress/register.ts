b02b5601a0b9e26aed3d1a86d911a263a5f6c068
duiliuliu <pengrui55555@163.com>
2026-04-30 23:20:58 +0000

    Revert commits eadaf2b..8f64233
    This reverts 4 commit(s) from eadaf2b to 8f64233.
    
    Reverted commits:
    8f64233 feat: sync latest updates
    884eb72 Revert commits eadaf2b..0e1bcf9
    0e1bcf9 refactor: 调整 setTimeout 等待时间为 10 ms
    eadaf2b refactor: 提取 renderComponent 为公用工具函数，移除 ReactDOMServer.renderToStaticMarkup
    
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/lib/taskProgress/register.ts
+++ src/lib/taskProgress/register.ts
@@ -11,7 +11,6 @@ import { calculateTaskProgress } from './taskQuery'
 import { ProgressDisplayType } from './types'
 import { logseqAPI } from '../../logseq'
 import { getSettings } from '../../settings'
-import { SettingsProvider } from '../../settings/useSettings'
 import { logger } from '../logger/logger'
 
 const MACRO_PREFIX = ':taskprogress'
@@ -49,13 +48,11 @@ async function renderProgress(blockId: string, slot: string): Promise<boolean> {
     }
     
     const template = ReactDOMServer.renderToStaticMarkup(
-      React.createElement(SettingsProvider, null,
-        React.createElement(TaskProgressComponent, {
-          progressData,
-          displayType,
-          config: { ...config, showLabel, labelFormat },
-        })
-      )
+      React.createElement(TaskProgressComponent, {
+        progressData,
+        displayType,
+        config: { ...config, showLabel, labelFormat },
+      })
     )
     
     logseqAPI.provideUI({
