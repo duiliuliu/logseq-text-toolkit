239a0cdd4b38a59f4ceaf351b3bf6b288b60ca3e
duiliuliu <pengrui55555@163.com>
2026-06-05 07:50:17 +0000

    feat: 新增功能开关和动态过滤 Tab
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/components/SettingsModal/tabs/GeneralSettings.tsx
+++ src/components/SettingsModal/tabs/GeneralSettings.tsx
@@ -112,6 +112,18 @@ function GeneralSettings({ settings, setSettings, onSave, isSaving, language }: 
         />
       </div>
 
+      <div className="ltt-setting-item">
+        <label>{t('settings.developerMode', language)}</label>
+        <label className="ltt-switch">
+          <input
+            type="checkbox"
+            checked={settings.developerMode || false}
+            onChange={(e) => handleSettingChange('developerMode', e.target.checked)}
+          />
+          <span className="ltt-switch-slider"></span>
+        </label>
+      </div>
+
       {/* Feature Management Section */}
       <div className="ltt-feature-management">
         <div 
@@ -215,18 +227,6 @@ function GeneralSettings({ settings, setSettings, onSave, isSaving, language }: 
             )}
           </div>
         )}
-      </div>
-
-      <div className="ltt-setting-item">
-        <label>{t('settings.developerMode', language)}</label>
-        <label className="ltt-switch">
-          <input
-            type="checkbox"
-            checked={settings.developerMode || false}
-            onChange={(e) => handleSettingChange('developerMode', e.target.checked)}
-          />
-          <span className="ltt-switch-slider"></span>
-        </label>
       </div>
 
       <div className="ltt-settings-actions">
